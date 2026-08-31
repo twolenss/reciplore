ceaa63f1a7fbecf0b9e9ec87d6bb82f6ef0925a2
Tolens <francesztolentino002@gmail.com>
2026-09-01 02:36:25 +0800

    feat: Added the routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout"
 
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import RecipeDetails from "./pages/RecipeDetails";
+import SearchResults from "./pages/SearchResults";
+import Categories from "./pages/Categories";
+import CategoryRecipes from "./pages/CategoryRecipes";
+import Favorites from "./pages/Favorites";
 function App() {
+  return(
 
-  return (
-    <></>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+        <Route index element={<AppHome/>}/>
+
+        <Route path="recipes" >
+          <Route index element={<Recipes/>}/>
+          <Route path=":id" element={<RecipeDetails/>}/>
+        </Route>
+
+        <Route path="search" element={<SearchResults/>} />
+
+        <Route path="categories">
+          <Route index element={<Categories/>}/>
+          <Route path=":categoryname" element={<CategoryRecipes/>}/>
+        </Route>
+
+        <Route path="favorites" element={<Favorites/> }/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
